39eee7472401a8611ebd962e7e8df4cbeaf6b781
NetLamp-Y <oplpublishergerman@gmail.com>
2023-05-23 16:15:11 +0200

    You can now add views to a uiStack
    Use (uiPrintIntoStack: stack): view - this works very much like "uiPrint" for normal containers

--- APIs/html5ui/packages/uiStack.js
+++ APIs/html5ui/packages/uiStack.js
@@ -1,28 +1,29 @@
+//Wrap a view so it can be added to a stack view and look correct
+function createLayer(value){
+	var layer = document.createElement("div");
+	
+	var child = GLang.displayValue(value);
+	if(!child.style.pointerEvents) {
+		child.style.pointerEvents = "auto"
+	}
+	layer.appendChild(child);
+
+	layer.style.width = "100%";
+	layer.style.height = "100%";
+	layer.style.position = "absolute";
+	layer.style.top = 0;
+	layer.style.left = 0;
+	layer.style.pointerEvents = "none";
+	
+	return layer
+}
+
 GLang.defaultRuntimeEnvironment.setInnerVariable("uiStack", {value:function(env, args){
 	var div = document.createElement("div");
 	
 	var arr = args[0].value;
 	if(!(arr instanceof Array)){
 		arr = [args[0]];
-	}
-	
-	function createLayer(value){
-		var layer = document.createElement("div");
-		
-		var child = GLang.displayValue(value);
-		if(!child.style.pointerEvents) {
-			child.style.pointerEvents = "auto"
-		}
-		layer.appendChild(child);
-
-		layer.style.width = "100%";
-		layer.style.height = "100%";
-		layer.style.position = "absolute";
-		layer.style.top = 0;
-		layer.style.left = 0;
-		layer.style.pointerEvents = "none";
-		
-		return layer
 	}
 	
 	for(var i = 0; i < arr.length; i++){
@@ -33,3 +34,13 @@ GLang.defaultRuntimeEnvironment.setInnerVariable("uiStack", {value:function(env,
 	div.style.position = "relative"
 	return {value:div, display:"dom"};
 }, display:"function"});
+
+GLang.defaultRuntimeEnvironment.setInnerVariable("uiPrintIntoStack", {value:function(env, args){
+	var stack = args[0].value;
+	
+	return {value:function(env, args) {
+		stack.appendChild(createLayer(args[0]));
+		return GLang.voidValue;
+	}};
+}, display:"function"});
+
