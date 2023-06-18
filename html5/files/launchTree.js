90b940d304dcbaf25699acf1ec6da7681d3eccbf
NetLamp-Y <oplpublishergerman@gmail.com>
2023-06-18 13:05:28 +0200

    In finished HTML files, the entire app code is now in one place
    Before, the code was in two places - one part at the top, with the libraries and runtime definitions, and one part at the bottom, with the actual app code to run. This made the entire thing pretty hard to optimize, so now it is one single blob at the top.

---
+++ html5/files/launchTree.js
@@ -0,0 +1,37 @@
+GLang.launchTree = function(tree) {
+    "use strict";
+	//Timeout
+	var timeoutSince = parseInt(GLang.eval("storageLoadString: 'kalzit.timeoutSince'").value);
+	var timeoutActive = false;
+	var timeoutMinutes = parseInt(GLang.eval("storageLoadString: 'kalzit.timeoutMinutes'").value);
+	
+	if (timeoutSince) {
+		//Timeout is set - check what to do now
+		var diff = Date.now() - timeoutSince;
+		if(diff < 1000 * 60 * 20) {
+			//Twenty minutes are not up yet - do not allow using the app
+			timeoutActive = true;
+			GLang.eval("'kalzit.timeoutMinutes' storageSaveString 0");
+		} else {
+			//Timeout is over - reset minute count (allows 30 minutes of usage)
+			Timeout.startTimer(timeoutMinutes);
+		}
+	} else {
+		Timeout.startTimer(timeoutMinutes);
+	}
+	
+	if (!timeoutActive){
+		try {
+			GLang.evaluateTree(tree, GLang.appEnvironment = GLang.RuntimeEnvironment(GLang.defaultRuntimeEnvironment));
+		} catch (error) {
+			console.warn("Your app code crashed: ");
+			console.log(error);
+			console.log("---")
+		}
+		if(GLANG_DEBUG) {
+			console.warn("Your app code should be running now!");
+			//Start auto-refresh
+			GLang.eval("!debugAutoRefresh");
+		}
+	} else GLang.eval("{} showMessageAsync 'Your app timeout is currently active - you can use the Kalzit apps again in 20 minutes (or less)'")
+}
