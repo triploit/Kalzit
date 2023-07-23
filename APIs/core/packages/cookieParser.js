df602edf3f7bb19f232aedee2b35c458b01aeeea
NetLamp-Y <oplpublishergerman@gmail.com>
2023-07-23 16:22:49 +0200

    Removed the cookie parser from the core language
    This was never used anymore

--- APIs/core/packages/cookieParser.js
+++
@@ -1,2 +0,0 @@
-GLang.defaultRuntimeEnvironment.qdSet("parse_cookie_names", GLang.wrapJsToValue(GLang.listCookies));
-GLang.defaultRuntimeEnvironment.qdSet("parse_cookie_value", GLang.wrapJsToValue(GLang.getCookie));
