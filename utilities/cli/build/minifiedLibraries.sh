09ba9cd6338d10989248c3a9055f7ea8992d5b10
NetLamp-Y <oplpublishergerman@gmail.com>
2023-08-25 15:20:38 +0200

    Update minifiedLibraries.sh

--- utilities/cli/build/minifiedLibraries.sh
+++ utilities/cli/build/minifiedLibraries.sh
@@ -3,11 +3,7 @@ do
 	folder="$(dirname $packageIndex)"
 	echo "Minifying API $folder"
 	
-	# We use multiple processes to do stuff in parallel. To make sure this does not fail, limit the amount of parallel processes to a tenth of the user maximum
-	processLimit=$(ulimit -u)
-	processLimit=$((processLimit / 10))
-	while [ $(jobs -r | wc -l) -ge $processLimit ] ; do sleep 1 ; done
-	./cli run nodeApp $(pwd)/nodejs/files/minifyKalzitPackage.txt --input-file $folder/platform-packages.json --output-file $folder/_min.js &
+	./cli run nodeApp $(pwd)/nodejs/files/minifyKalzitPackage.txt --input-file $folder/platform-packages.json --output-file $folder/_min.js
 done
 
 wait
