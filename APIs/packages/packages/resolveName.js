866bfbd77d02f2ec667feb7ca400976c8dcb71f1
NetLamp-Y <oplpublishergerman@gmail.com>
2025-12-08 17:08:08 +0100

    Changed the way to create functions from strings
    The previous variant would bind the created function to the scope it was created in, which is a) dangerous because you might make unwanted variables accessible, and b) harder to optimize / translate to other languages.
    
    Now, noParameterFunctionFromString is no longer available; instead, there is globalSingleParameterFunctionFromString, which is always run with only the global scope as its parent, and any variables you really need to pass through can be packaged into the single parameter (which you can name however you like).
    
    For an example of how to use this, see the changes in nodejs/files/httpServer.k

--- APIs/packages/packages/resolveName.js
+++ APIs/packages/packages/resolveName.js
@@ -12,9 +12,17 @@ GLang.dr.qdSet("resolve_name", {value:function(env, args) {
 	}
 }});
 
-GLang.dr.qdSet("no_parameter_function_from_string", {value:function(env, args) {
-	const tree = GLang.prepareTree(GLang.generateTree(args[0].value));
-	return {value:function() {
-		return GLang.evaluatePreparedTree(tree, env);
+GLang.dr.qdSet("global_single_parameter_function_from_string", {value:function(env, args) {
+	const tree = GLang.prepareTree(GLang.generateTree(args[1].value));
+	const argName = args[0].value;
+
+	return {value:function(env, args) {
+		const newEnv = Object.create(GLang.dr);
+
+		//console.log("globalSingleParameterFunctionFromString: argName is " + argName);
+		//console.log("globalSingleParameterFunctionFromString: value is " + GLang.stringify(args[0]));
+		
+		newEnv[argName] = args[0];
+		return GLang.evaluatePreparedTree(tree, newEnv);
 	}}
 }})
