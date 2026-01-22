fd00f38ce4c2dcbf5132ce21783a6a7b91a19cd7
NetLamp-Y <oplpublishergerman@gmail.com>
2026-01-22 14:06:16 +0100

    Added assertions that can deliberately crash apps

---
+++ APIs/crashing/packages/assert.js
@@ -0,0 +1,10 @@
+this.assert_or_crash = function(shouldBeOne, errorMessage) {
+	if(shouldBeOne !== 1) {
+		//We have to produce an error
+		if("string" == typeof errorMessage) {
+			throw new Error("An assertion failed; " + errorMessage);
+		} else {
+			throw new Error("An assertion failed");
+		}
+	}
+}
